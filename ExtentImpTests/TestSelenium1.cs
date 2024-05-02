042e94c94b84aeb22df5f461c4be5a0272bbe3cb
awana01 <automationsingh.1983@gmail.com>
2024-05-02 11:12:19 +0530

    commit yml with single test 11.12 AM

--- ExtentImpTests/TestSelenium1.cs
+++ ExtentImpTests/TestSelenium1.cs
@@ -12,6 +12,7 @@ public class TestSelenium1 : BaseTest{
         IWebElement searchTextBox = driver.FindElement(By.Id("twotabsearchtextbox"));
         searchTextBox.SendKeys("men's denim");
         driver.FindElement(By.Id("nav-search-submit-text")).Click();
+        Thread.Sleep(5000);
         // string _totalResults = driver.FindElement(By.CssSelector("div.a-section.a-spacing-small.a-spacing-top-small")).Text;
         string _totalResults = driver.FindElement(By.XPath("//span[@data-component-type='s-result-info-bar']/div/h1/div/div[1]")).Text;
         Console.WriteLine(String.Format("Results Found: {0}",_totalResults));
